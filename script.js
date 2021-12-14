2e056d53dab08d14ef8b28f1fa44d4494208a296
JIM\Администратор <vladislav.fedurkin@gmail.com>
2021-12-14 17:01:07 +0300

    init: add quote

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+class Quote {
+  index = 0;
+  quotes = [
+    {
+      "text": "Пишите код так, как будто сопровождать его будет склонный к насилию психопат, который знает, где вы живете",
+      "author": "Стив Макконнелл"
+    },
+    {
+      "text": "Сложность программы растет до тех пор, пока не превысит способности программиста",
+      "author": "Артур Блох. Законы Мэрфи"
+    },
+      {
+      "text": "Lisp — это не язык, а строительный материал.",
+      "author": "Alan Kay"
+    },
+    {
+      "text": "Низкоуровневый язык — это когда требуется внимание к вещам, которые никак не связаны с программами на этом языке.",
+      "author": "Alan J. Perlis"
+    },
+    {
+      "text": "В хорошем дизайне добавление вещи стоит дешевле, чем сама эта вещь.",
+      "author": "Thomas C. Gale"
+    },
+    {
+      "text": "Болтовня ничего не стоит. Покажите мне код.",
+      "author": "Linus Torvalds"
+    }
+  ];
+  getQuote(){
+    if(this.index > this.quotes.length-1) {
+      this.index = 0;
+    }
+    return this.quotes[this.index++]
+  }
+}
+
 const time = document.querySelector(".time");
 const dateContainer = document.querySelector(".date");
 const greeting = document.querySelector(".greeting");
@@ -8,7 +44,17 @@ const buttonPrev = document.querySelector(".slide-prev");
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
-const city = document.querySelector('.city');
+const cityInput = document.querySelector('.city');
+const changeQuote = document.querySelector('.change-quote');
+const quoteContainer = document.querySelector('.quote');
+const authorContainer = document.querySelector('.author');
+const playPrev = document.querySelector('.play-prev');
+const playMusic = document.querySelector('.play');
+const playNext = document.querySelector('.play-next');
+const playListName = document.querySelector('.play-list');
+cityInput.value = 'Minsk';
+const quote = new Quote()
+
 
 function showTime() {
   const date = new Date();
@@ -50,9 +96,9 @@ function getLocalStorage() {
 window.addEventListener('load', getLocalStorage)
 
 function getRandomNum() {
-  let max = 20;
+  let max = 19;
   let min = 1;
-  return num =  Math.floor(Math.random() * (max - min + 1)) + min;
+  return num =  Math.ceil(Math.random() * (max - min + 1)) + min;
 }
 const randomNum = getRandomNum()
 
@@ -85,10 +131,9 @@ buttonNext.addEventListener('click', () => getSlideNext());
 
 
 async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=b3b50f00d9a2c5d1c6c0c92622466732&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&lang=ru&appid=b3b50f00d9a2c5d1c6c0c92622466732&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
-  
   weatherIcon.className = 'weather-icon owf';
   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
   temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
@@ -100,5 +145,61 @@ function setCity(event) {
     return getWeather();
   }
 }
-document.addEventListener('DOMContentLoaded', getWeather);
-city.addEventListener('keypress', setCity);
+document.addEventListener('DOMContentLoaded', () => {
+  getWeather()
+  const quoteConfig = quote.getQuote();
+  quoteContainer.textContent = quoteConfig.text
+  authorContainer.textContent = quoteConfig.author
+});
+cityInput.addEventListener('keypress', (e) => setCity(e));
+
+changeQuote.addEventListener('click', () => {
+  const quoteConfig = quote.getQuote();
+  quoteContainer.textContent = quoteConfig.text
+  authorContainer.textContent = quoteConfig.author
+});
+
+
+
+
+const audio = new Audio();
+
+function playAudio() {
+//   const music = 
+//   audio.src = `./assets/sounds/${music}`;
+//   audio.currentTime = 0;
+//   audio.play();
+}
+
+// function pauseAudio() {
+//   audio.pause();
+//   playMusic.classList.toggle('pause');
+// }
+
+playMusic.addEventListener('click', playAudio, () => {
+  console.log('event')
+  playMusic.classList.toggle('pause');
+});
+
+const playList = [
+  {
+    title: 'Aqua Caelestis',
+    src: '../assets/sounds/Aqua Caelestis.mp3',
+    duration: '00:58'
+  },
+  {
+    title: 'Ennio Morricone',
+    src: '../assets/sounds/River Flows In You.mp3',
+    duration: '03:50'
+  },
+  { 
+    title: 'River Flows In You',
+    src: '../assets/sounds/River Flows In You.mp3',
+    duration: '03:50'
+  },
+  {
+    title: 'Summer Wind',
+    src: '../assets/sounds/River Flows In You.mp3',
+    duration: '03:50'
+  }
+];
